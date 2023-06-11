b468b69fb32cb1cea6d7f764f389a90c9d759469
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2023-06-12 01:13:38 +0900

    링크 복사 기능 추가

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 import { getAuth, signOut } from "firebase/auth";
 import { FaUserCircle, FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
+import { CopyToClipboard } from "react-copy-to-clipboard/src";
 import {
   getFirestore,
   collection,
@@ -14,11 +15,12 @@ import {
 } from "firebase/firestore";
 import app from "../firebase";
 import cakeImg from "../images/cakeImg.png";
+
 const Container = styled.div`
   width: 100%;
   height: 100vh;
 `;
-const MainPannel = styled.div`
+const MainPanel = styled.div`
   height: 100vh;
   width: 100%;
   align-items: center;
@@ -65,12 +67,23 @@ const ImageBox = styled.div`
   width: 300px;
   height: 500px;
   align-items: center;
-  background-color: black;
 `;
-
+const MessagePanel = styled.div`
+  position: absolute;
+  margin: 30px 0px;
+  left: 50%;
+  transform: translate(-50%);
+`;
+const MessageBox = styled.div`
+  width: 300px;
+  height: 400px;
+  border-radius: 15px;
+  background-color: gray;
+`;
 const Home = ({ userObj }) => {
   const auth = getAuth();
   const db = getFirestore(app);
+  const [linkId, setLinkId] = useState("");
   const [loading, setLoading] = useState(true);
   const [isUserLink, setIsUserLink] = useState(false);
   const [contents, setContents] = useState("");
@@ -78,12 +91,14 @@ const Home = ({ userObj }) => {
   const [isBirthDay, setIsBirthDay] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [length, setLength] = useState(0);
+  const [openMsg, setOpenMsg] = useState(false);
 
   const checkUserLink = async () => {
     const querySnapshot = await getDocs(collection(db, "availableID"));
     querySnapshot.forEach((doc) => {
       if (userObj.uid === doc.data().userId) {
         setIsUserLink(true);
+        setLinkId(doc.data().linkId);
       }
     });
   };
@@ -91,12 +106,13 @@ const Home = ({ userObj }) => {
   const makeLink = async () => {
     const v4 = uuidv4();
     if (isUserLink) {
-      console.log("alreay exist");
+      alert("alreay exist");
     } else {
       const docRef = await addDoc(collection(db, "availableID"), {
         userId: userObj.uid,
         linkId: v4,
       });
+      setLinkId(v4);
     }
     console.log(`localhost:3000/user/${v4}`);
   };
@@ -136,7 +152,6 @@ const Home = ({ userObj }) => {
     checkUserLink();
     getMessages();
     getBirthDay();
-
     setLoading(false);
   }, []);
 
@@ -144,7 +159,7 @@ const Home = ({ userObj }) => {
     <div>loading...</div>
   ) : (
     <Container>
-      <MainPannel isopen={isOpen}>
+      <MainPanel isopen={isOpen}>
         <Header>
           <div style={{ width: 24, marginLeft: "15px" }} />
           <Text>소원을 말해봐</Text>
@@ -181,89 +196,97 @@ const Home = ({ userObj }) => {
                     top: "70px",
                   }}
                 />
-                <Text style={{ position: "fixed", top: "72px", right: "20px" }}>
+                <Text
+                  style={{
+                    fontSize: "20px",
+                    position: "fixed",
+                    top: "72px",
+                    right: "20px",
+                  }}
+                >
                   {userObj.displayName}
                   {"\n"}
                   {birthDay}
                 </Text>
               </div>
+              <div
+                style={{ height: "400px", width: "200px", textAlign: "center" }}
+              >
+                <Text
+                  style={{ padding: "10px 0px" }}
+                  onClick={() => {
+                    signOut(auth);
+                  }}
+                >
+                  로그아웃
+                </Text>
+              </div>
             </>
           ) : null}
         </MenuBar>
+        {openMsg ? (
+          <MessagePanel>
+            <MessageBox>
+              <AiOutlineClose
+                size={24}
+                style={{ position: "fixed", right: "15px", top: "20px" }}
+                onClick={() => {
+                  setOpenMsg((prev) => !prev);
+                }}
+              />
+            </MessageBox>
+          </MessagePanel>
+        ) : null}
+
         <ContentBox>
-          <Text>지금까지 {length}개의 초가 밝혀졌어요!</Text>
+          <div style={{ textAlign: "left" }}>
+            <Text>{userObj.displayName} 님의 케이크에</Text>
+            <Text>지금까지 {length}개의 초가 밝혀졌어요!</Text>
+          </div>
           <ImageBox>
             <img
               src={cakeImg}
               style={{
                 height: "300px",
                 width: "300px",
-                backgroundColor: "red",
               }}
             />
           </ImageBox>
-
-          <button onClick={makeLink}>make link</button>
-          <button
-            onClick={() => {
-              signOut(auth);
-            }}
-          >
-            LogOut
-          </button>
+          {isUserLink ? (
+            <CopyToClipboard
+              text={`localhost:3000/user/${linkId}`}
+              onCopy={() => {
+                alert("복사완료!");
+              }}
+            >
+              <Text>링크 복사하기</Text>
+            </CopyToClipboard>
+          ) : (
+            <button onClick={makeLink}>
+              <Text>링크 공유하기</Text>
+            </button>
+          )}
           {isBirthDay ? (
-            <div>
-              {contents &&
+            <button
+              onClick={() => {
+                setOpenMsg((prev) => !prev);
+              }}
+            >
+              {/* {contents &&
                 contents.map((data, index) => (
                   <div key={index}>
                     {data.name} / {data.content}
                   </div>
-                ))}
-            </div>
+                ))} */}
+              메세지 확인하기
+            </button>
           ) : (
             <div>not yet</div>
           )}
         </ContentBox>
-      </MainPannel>
+      </MainPanel>
     </Container>
   );
 };
 
 export default Home;
-
-{
-  /* <MenuBar isopen={isOpen}>
-  {isOpen ? (
-    <>
-      <AiOutlineClose
-        size={24}
-        style={{ position: "fixed", right: "0", top: "20px" }}
-        onClick={() => {
-          setIsOpen((prev) => !prev);
-        }}
-      />
-      <div
-        style={{
-          height: "150px",
-          width: "180px",
-          margin: "0px 15px",
-          borderBottom: "1px solid black",
-        }}
-      >
-        <FaUserCircle
-          size={55}
-          style={{
-            position: "fixed",
-            top: "70px",
-          }}
-        />
-        <Text style={{ position: "fixed", top: "72px" }}>
-          {userObj.displayName}
-          {"\n"}
-          {birthDay}
-        </Text>
-      </div>
-    </>
-  ) : null}
-</MenuBar>; */
-}
